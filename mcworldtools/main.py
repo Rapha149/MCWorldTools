321045c2b349a17b21bb84d5353d3a3039269c4d
Rapha149 <raphael@raphael-jantzen.de>
2022-02-14 12:24:24 +0100

    Add more command line arguments.

--- mcworldtools/main.py
+++ mcworldtools/main.py
@@ -1,5 +1,6 @@
-import sys
+from argparse import ArgumentParser
 from pathlib import Path
+from re import A
 from nbt.nbt import *
 
 try:
@@ -7,20 +8,39 @@ try:
 except ImportError:
    from tools import remove_unused_chunks, find_blocks, find_command_blocks, find_entities
 
+available_tools = ['Remove unused chunks', 'Find blocks', 'Find command blocks', 'Find entities']
+
 def main():
+   parser = ArgumentParser(prog='mcworldtools',
+                           description='MCWorldTools by Rapha149',
+                           allow_abbrev=False)
+   parser.add_argument('-w', '--world', help='Use a different world folder than the current working directory.')
+   parser.add_argument('-t', '--tool', type=int, choices=range(1, len(available_tools) + 1), help='Choose the tool you want to use beforehand')
+   parser.add_argument('-o', '--output', help='Select a file to write the output statistics to.')
+   parser.add_argument('-f', '--output-format', choices=['json', 'yaml'], default='json', help='The output file format. May be "json" (default) or "yaml".')
+   parser.add_argument('--confirm', action='store_true', help='Automatically confirm any confirmation requests.')
+   args = parser.parse_args()
+
    world_folder = Path.cwd()
-   if len(sys.argv) >= 2:
-      world_folder = Path(sys.argv[1])
+   if args.world:
+      world_folder = Path(args.world)
       if not world_folder.is_dir():
          print(f'The folder "{sys.argv[1]}" does not exist.')
          exit(1)
 
+   output_file = None
+   if args.output:
+      output_file = Path(args.output)
+      if output_file.is_dir():
+         print(f'The output file "{output_file.name}" is a folder.')
+         exit(1)
+
    level_dat = Path(world_folder, 'level.dat')
    if not level_dat.is_file():
-      if len(sys.argv) >= 2:
+      if args.world:
          print('That is not a world folder.')
       else:
-         print('Please run in a world folder or state a world folder as argument.')
+         print('Please run in a world folder or use the option "--world".')
       exit(2)
    
    level = NBTFile(fileobj=level_dat.open('rb'))
@@ -29,36 +49,39 @@ def main():
       version = f' (Version: {data["Version"]["Name"]})' if 'Version' in data else ''
       print(f'Detected world: "{data["LevelName"]}"{version}')
    except KeyError:
-      print('This is not a valid world folder.')
+      print(('That' if args.world else 'This') + ' is not a valid world folder.')
       exit(2)
 
-   print('Select which tool you want to use.'
-      '\n1. Remove unused chunks'
-      '\n2. Find blocks'
-      '\n3. Find command blocks'
-      '\n4. Find entities')
 
-   tool = None
-   while True:
-      answer = input('Select a tool (1-4): ')
-      if not answer.isnumeric():
-         print('Please state a number.')
-         continue
-      
-      tool = int(answer)
-      if tool < 1 or tool > 4:
-         print('Please state a number between 1 and 4.')
-         continue
-      break
+   tool = args.tool
+   if not tool:
+      print('Select which tool you want to use.'
+         '\n1. Remove unused chunks'
+         '\n2. Find blocks'
+         '\n3. Find command blocks'
+         '\n4. Find entities')
+
+      while True:
+         answer = input('Select a tool (1-4): ')
+         if not answer.isnumeric():
+            print('Please state a number.')
+            continue
+         
+         tool = int(answer)
+         if tool < 1 or tool > 4:
+            print('Please state a number between 1 and 4.')
+            continue
+         break
+   print(f'Using tool "{available_tools[tool - 1]}"')
 
    if tool == 1:
-      remove_unused_chunks.start(world_folder)
+      remove_unused_chunks.start(world_folder, output_file, args.output_format, args.confirm)
    elif tool == 2:
-      find_blocks.start(world_folder)
+      find_blocks.start(world_folder, output_file, args.output_format, args.confirm)
    elif tool == 3:
-      find_command_blocks.start(world_folder)
+      find_command_blocks.start(world_folder, output_file, args.output_format, args.confirm)
    elif tool == 4:
-      find_entities.start(world_folder)
+      find_entities.start(world_folder, output_file, args.output_format, args.confirm)
 
 if __name__ == '__main__':
    main()
--- mcworldtools/tools/remove_unused_chunks.py
+++ mcworldtools/tools/remove_unused_chunks.py
@@ -1,4 +1,5 @@
 import time
+import json, yaml
 from tqdm import tqdm
 from chunk import Chunk
 from pathlib import Path
@@ -11,24 +12,25 @@ def list_files(dir):
 def get_size(files):
    return sum(file.stat().st_size for file in files)
 
-def start(world_folder):
+def start(world_folder, output_file, output_format, confirm):
    region_folder = Path(world_folder, 'region')
    if not region_folder.is_dir():
       print('No region folder was found.')
       exit(3)
 
-   print('\nWarning: This operation will remove all chunks in which no player was present.'
-         '\nTherefore, chunks with changed blocks may be removed since players can change blocks even if they are not in the chunk.'
-         '\nIt is recommended to make a backup of your world beforehand.'
-         '\nNo further confirmation requests will be made before chunks are removed.')
-   while True:
-      answer = input('Do you want to continue? (y/N): ')
-      if not answer or answer.lower() == 'n':
-         exit()
-      elif answer.lower() != 'y':
-         print('Please state "y" or "n".')
-      else:
-         break
+   if not confirm:
+      print('\nWarning: This operation will remove all chunks in which no player was present.'
+            '\nTherefore, chunks with changed blocks may be removed since players can change blocks even if they are not in the chunk.'
+            '\nIt is recommended to make a backup of your world beforehand.'
+            '\nNo further confirmation requests will be made before chunks are removed.')
+      while True:
+         answer = input('Do you want to continue? (y/N): ')
+         if not answer or answer.lower() == 'n':
+            exit()
+         elif answer.lower() != 'y':
+            print('Please state "y" or "n".')
+         else:
+            break
 
    files = list_files(region_folder)
    size = get_size(files)
@@ -67,18 +69,44 @@ def start(world_folder):
             region_file.unlink()
             pbar.update(delete_count)
 
-   elapsed_seconds = int(time.time() - start_time)
+   elapsed_time = int(round((time.time() - start_time) * 1000))
+   elapsed_seconds = int(elapsed_time / 1000)
    elapsed_minutes = int(elapsed_seconds / 60)
    elapsed_seconds %= 60
+   human_readable_elapsed_time = f'{elapsed_minutes}m {elapsed_seconds}s'
 
    new_size = get_size(list_files(region_folder))
    freed_space_unit = 'kB'
-   freed_space = (size - new_size) / 1000
+   raw_freed_space = size - new_size
+   freed_space = raw_freed_space / 1000
    if freed_space >= 1000:
       freed_space /= 1000
       freed_space_unit = 'MB'
    if freed_space >= 1000:
       freed_space /= 1000
       freed_space_unit = 'GB'
+   human_readable_freed_space = f'{freed_space:.2f}{freed_space_unit}'
 
-   print(f'\nRemoved {count}/{total} ({count/total*100:0.2f}%) chunks. (Elapsed time: {elapsed_minutes}m {elapsed_seconds}s; Freed space: {freed_space:.2f}{freed_space_unit})')
+   print(f'\nRemoved {count}/{total} ({count/total*100:0.2f}%) chunks. (Elapsed time: {human_readable_elapsed_time}; Freed space: {human_readable_freed_space})')
+
+   if output_file:
+      data = {
+         'chunks': {
+            'removed': count,
+            'total': total
+         },
+         'elapsed_time': {
+            'raw': elapsed_time,
+            'human_readable': human_readable_elapsed_time
+         },
+         'freed_space': {
+            'raw': raw_freed_space,
+            'human_readable': human_readable_freed_space
+         }
+      }
+      with output_file.open('w') as file:
+         if output_format == 'json':
+            json.dump(data, file, indent=3)
+         elif output_format == 'yaml':
+            yaml.dump(data, file, indent=3)
+         print(f'\nSaved output to "{output_file.name}"')
